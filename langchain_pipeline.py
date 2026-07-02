32bb31cb63254cd9476508a649f60dd0d824894d
Krishna Kumar <142143663+krishna-kr-1@users.noreply.github.com>
2026-07-02 12:30:27 +0530

    Update model version in ChatGoogleGenerativeAI
    Add using of .env file for secure use of api_key

--- langchain_pipeline.py
+++ langchain_pipeline.py
@@ -7,7 +7,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 google_api_key = os.getenv("GOOGLE_API_KEY")
 
-llm = ChatGoogleGenerativeAI( model = "gemini-1.5-flash" , google_api_key = google_api_key)
+llm = ChatGoogleGenerativeAI( model = "gemini-2.5-flash" , google_api_key = google_api_key)
 
 summary_template = PromptTemplate(
     input_variables = ["document"],
